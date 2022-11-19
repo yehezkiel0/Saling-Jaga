f7f22ae227c7da1e400ca07bb09877f4db60069b
Muchammad Saifurrijaal <88447746+msaifurrijaal@users.noreply.github.com>
2022-11-19 11:00:24 +0700

    update baru

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -78,10 +78,13 @@
                     <div id="dropdown"
                         class="hidden absolute right-0 top-10 w-fit btn-logout z-10 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
                         <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
-                            <li>
-                                <button
-                                    class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Logout</button>
-                            </li>
+                            <form action="/logout" method="POST">
+                                @csrf
+                                <li>
+                                    <button
+                                        class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" type="submit">Logout</button>
+                                </li>
+                            </form>
                         </ul>
                     </div>
 
